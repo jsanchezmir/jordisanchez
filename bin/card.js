945c975eabfaa9536c93a4535a7c3124b914992a
Jordi Sanchez <jordisanchezmir@gmail.com>
2020-12-18 01:11:47 +0100

    feat: Release 1.1.0

--- bin/card.js
+++ bin/card.js
@@ -12,29 +12,40 @@ const logOptions = {
 
 const content = {
     personalInformation: {
-        name: chalk.white.bold('Jordi Sánchez Mir'),
+        name: chalk.greenBright.bold('Jordi'),
+        surname: chalk.greenBright.bold('Sánchez Mir'),
     },
     position: {
         title: chalk.blackBright('Position:'),
-        text: chalk.white.bold('Frontend Team Lead at Seat:CODE')
+        text: chalk.white.bold('Frontend at Seat:CODE')
+    },
+    email: {
+        title: chalk.blackBright('Email:'),
+        text: chalk.white.bold('jordi.sanchez@code.seat')
+    },
+    mobile: {
+        title: chalk.blackBright('Mobile:'),
+        text: chalk.white.bold('+34 669 510 810')
     },
     github: {
         title: chalk.blackBright('Github:'),
-        text: chalk.green.bold('github.com/j00ye')
+        text: chalk.white.bold('github.com/jordisanchez-seatcode')
     },
     linkedin: {
         title: chalk.blackBright('Linkedin:'),
-        text: chalk.green.bold('linkedin.com/in/jordisanchezmir')
+        text: chalk.white.bold('linkedin.com/in/jordisanchezmir')
     },
 }
 
-function tranformContentToString(content){
+function tranformContentToString(content) {
     const endline = '\n';
     const space = ' ';
-    return content.personalInformation.name + endline + endline +
+    return content.personalInformation.name + space + content.personalInformation.surname + endline + endline +
         content.position.title + space + content.position.text + endline +
-        content.github.title + space + content.github.text + endline +
+        content.email.title + space + space + space + space + content.email.text + endline +
+        content.mobile.title + space + space + space + content.mobile.text + endline +
+        content.github.title + space + space + space + content.github.text + endline +
         content.linkedin.title + space + content.linkedin.text;
 }
-    
+
 console.log(boxen(tranformContentToString(content), logOptions));
